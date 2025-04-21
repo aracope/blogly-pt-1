dc3e14126a914e8e8fecbea904fced6c3fbb2de8
aracope <156398622+aracope@users.noreply.github.com>
2025-04-21 14:00:25 -0700

    Basic format done for the blogly exercise.

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 """Blogly application."""
 
 from flask import Flask
-from models import db, connect_db
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "shh-this-is-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+
+with app.app_context():
+    db.create_all()
+
